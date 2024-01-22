e476b37c02d925a9340ff51dba39e3e7fc8fe6c9
Guilherme Ferreira <guilherme.ferreira@farfetch.com>
2024-01-22 16:36:47 +0000

    docs: add docusaurus website

---
+++ website/docusaurus.config.js
@@ -0,0 +1,131 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'FARFETCH LoadShedding',
+  tagline: 'A .NET LoadShedding Library',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://farfetch.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Farfetch', // Usually your GitHub org/user name.
+  projectName: 'loadshedding', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/farfetch/loadhsedding/tree/main/website/',
+        },
+        blog: false, 
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+      },
+      navbar: {
+        logo: {
+          alt: 'LoadShedding',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://github.com/farfetch/loadshedding',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Introduction',
+                to: '/',
+              },
+              {
+                label: 'Getting Started',
+                to: '/category/getting-started',
+              },
+              {
+                label: 'Guides',
+                to: '/category/guides',
+              }
+            ]
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/loadshedding',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'FARFETCH Blog',
+                to: 'https://farfetchtechblog.com',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/farfetch/loadshedding',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} FARFETCH UK Limited. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+        additionalLanguages: ['csharp']
+      },
+    }),
+};
+
+export default config;
