6e516b983feafde5e27c99546fb54852be1850ee
Ailton Silva <ailton.silva@farfetch.com>
2024-04-22 08:36:00 -0300

    chore: unit test refactoring

--- tests/unit-tests/Farfetch.LoadShedding.Tests/Limiters/AdaptativeConcurrencyLimiterTests.cs
+++ tests/unit-tests/Farfetch.LoadShedding.Tests/Limiters/AdaptativeConcurrencyLimiterTests.cs
@@ -37,11 +37,12 @@ namespace Farfetch.LoadShedding.Tests.Limiters
             // Arrange
             var exceptedException = new Exception("Something went wrong");
             var exceptionTask = Task.Run(() => throw exceptedException);
-            var events = new LoadSheddingEvents();
-            var limitCalculatorMock = new Mock<ILimitCalculator>();
-            var queueSizeCalculatorMock = new Mock<IQueueSizeCalculator>();
 
-            var target = new AdaptativeConcurrencyLimiter(new ConcurrencyOptions(), limitCalculatorMock.Object, queueSizeCalculatorMock.Object, events);
+            var target = new AdaptativeConcurrencyLimiter(
+                new ConcurrencyOptions(),
+                Mock.Of<ILimitCalculator>(),
+                Mock.Of<IQueueSizeCalculator>(),
+                new LoadSheddingEvents());
 
             // Act
             var exception = await Assert.ThrowsAsync<AggregateException>(() => target.ExecuteAsync(() => exceptionTask));
