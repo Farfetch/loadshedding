4ea6d7f98743a8df8a222e12e11dd568ff293f97
Ailton Silva <ailton.silva@farfetch.com>
2024-04-22 08:19:55 -0300

    fix: loadshedding hidding errors

--- tests/unit-tests/Farfetch.LoadShedding.Tests/Limiters/AdaptativeConcurrencyLimiterTests.cs
+++ tests/unit-tests/Farfetch.LoadShedding.Tests/Limiters/AdaptativeConcurrencyLimiterTests.cs
@@ -30,5 +30,23 @@ namespace Farfetch.LoadShedding.Tests.Limiters
             // Assert
             Assert.True(wasInvoked);
         }
+
+        [Fact]
+        public async Task ExecuteAsync_WhenThereIsException_ThrowTheException()
+        {
+            // Arrange
+            var exceptedException = new Exception("Something went wrong");
+            var events = new LoadSheddingEvents();
+            var limitCalculatorMock = new Mock<ILimitCalculator>();
+            var queueSizeCalculatorMock = new Mock<IQueueSizeCalculator>();
+
+            var target = new AdaptativeConcurrencyLimiter(new ConcurrencyOptions(), limitCalculatorMock.Object, queueSizeCalculatorMock.Object, events);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<Exception>(() => target.ExecuteAsync(() => throw exceptedException));
+
+            // Assert
+            Assert.Equal(exceptedException, exception);
+        }
     }
 }
